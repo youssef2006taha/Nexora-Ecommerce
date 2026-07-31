7c10ebcc11b13a9ae010bdb4d9653bcdb665ae4f
Youssef Taha <y2006t2006a2006@gmail.com>
2026-07-31 21:57:54 +0300

    edit 5.3

--- store/src/api/axios.js
+++ store/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiInstance = axios.create({
-  baseURL: 'http://e-commerce-api-3wara.vercel.app', 
+  baseURL: 'https://e-commerce-api-3wara.vercel.app', 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
